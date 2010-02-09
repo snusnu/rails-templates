57922905bbe528630babc33c367d9aafd754e595
snusnu <gamsnjaga@gmail.com>
2010-02-09 06:53:07 +0100

    Try with version requirement for rspec

--- rspec_master_gemfile.rb
+++ rspec_master_gemfile.rb
@@ -7,10 +7,10 @@ git 'git://github.com/rspec/rspec-mocks.git'
 git 'git://github.com/rspec/rspec-rails.git'
 
 group(:test) do
-  gem 'rspec'
-  gem 'rspec-core'
-  gem 'rspec-expectations'
-  gem 'rspec-mocks'
-  gem 'rspec-rails'
+  gem 'rspec',              '2.0.0.a6'
+  gem 'rspec-core',         '2.0.0.a6'
+  gem 'rspec-expectations', '2.0.0.a6'
+  gem 'rspec-mocks',        '2.0.0.a6'
+  gem 'rspec-rails',        '2.0.0.a6'
 end
 GEMFILE
