22ed270f219e0cb1e55677636950a3e23860496e
snusnu <gamsnjaga@gmail.com>
2010-02-09 07:00:15 +0100

    Depend on what's available

--- rspec_master_gemfile.rb
+++ rspec_master_gemfile.rb
@@ -7,10 +7,10 @@ git 'git://github.com/rspec/rspec-mocks.git'
 git 'git://github.com/rspec/rspec-rails.git'
 
 group(:test) do
-  gem 'rspec',              '2.0.0.a6'
-  gem 'rspec-core',         '2.0.0.a6'
-  gem 'rspec-expectations', '2.0.0.a6'
-  gem 'rspec-mocks',        '2.0.0.a6'
+  gem 'rspec',              '2.0.0.a5'
+  gem 'rspec-core',         '2.0.0.a5'
+  gem 'rspec-expectations', '2.0.0.a5'
+  gem 'rspec-mocks',        '2.0.0.a5'
   gem 'rspec-rails',        '2.0.0.a6'
 end
 GEMFILE
