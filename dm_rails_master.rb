f7438e413ee04401d079e16b0c8352e8641bb2f3
snusnu <gamsnjaga@gmail.com>
2010-02-09 06:08:54 +0100

    Fixed link to rails_master_gemfile

--- dm_rails_master.rb
+++ dm_rails_master.rb
@@ -1,2 +1,2 @@
-apply 'http://github.com/snusnu/rails-templates/raw/rspec/rails_rspec_gemfile.rb'
+apply 'http://github.com/snusnu/rails-templates/raw/rspec/rails_master_gemfile.rb'
 apply 'http://github.com/snusnu/rails-templates/raw/rspec/rails_application.rb'
