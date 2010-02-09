5282196eb91db8da035d765de5d566264bb6ef38
snusnu <gamsnjaga@gmail.com>
2010-02-09 08:43:53 +0100

    Try auto_migrate! (all but 2 specs pass)

--- sample_scaffold.rb
+++ sample_scaffold.rb
@@ -1,6 +1,6 @@
 run 'vendor/bin/rake db:create'
 run 'vendor/bin/rails generate scaffold Person name:string'
-run 'vendor/bin/rake db:autoupgrade'
+run 'vendor/bin/rake db:automigrate'
 
 say ''
 say '--------------------------------------------------------------------------'
