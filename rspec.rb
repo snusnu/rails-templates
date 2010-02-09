73b9eddd8d7ec1119fcf508c0ba84dc6b646ead2
snusnu <gamsnjaga@gmail.com>
2010-02-09 08:33:22 +0100

    Lock the bundle before using it
    Hopefully this will help running rake spec as this
    is currently complaining that it can't find bundler
    because it falls back to rubygems. It does this
    because it's not running from .bundle/environment
    because this only gets created when the bundle gets
    locked.

--- rspec.rb
+++ rspec.rb
@@ -1,11 +1,13 @@
 # TODO this always uses rspec master for now. Implement interactive templates!
 apply 'http://github.com/snusnu/rails-templates/raw/rspec/rspec_master_gemfile.rb'
 
-run('bundle install vendor')
+run 'bundle install vendor'
 
-run('vendor/bin/rails generate rspec:install')
+run 'vendor/bin/rails generate rspec:install'
 
 apply 'http://github.com/snusnu/rails-templates/raw/rspec/sample_scaffold.rb'
 
-run('rspec spec')
-run('vendor/bin/rake spec')
+run 'bundle lock'
+
+run 'rspec spec'
+run 'vendor/bin/rake spec'
