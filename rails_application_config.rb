71d91e653d640865d642c1cd67f9304141e6dec9
snusnu <gamsnjaga@gmail.com>
2010-02-10 05:13:32 +0100

    Don't use action_mailer by default

--- rails_application_config.rb
+++ rails_application_config.rb
@@ -3,8 +3,12 @@ gsub_file 'config/application.rb', /require 'rails\/all'/ do
 # Pick the frameworks you want:
 require 'action_controller/railtie'
 require 'rails3_datamapper/railtie'
-require 'action_mailer/railtie'
+# require 'action_mailer/railtie'
 # require 'active_resource/railtie'
 # require 'rails/test_unit/railtie'
 RUBY
+end
+
+gsub_file 'config/application.rb', /#{config.action_mailer.raise_delivery_errors = false}/ do
+  "# config.action_mailer.raise_delivery_errors = false"
 end
