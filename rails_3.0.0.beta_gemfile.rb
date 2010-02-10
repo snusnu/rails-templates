8f1ac11a85235a1ee3a3079bd2be3a5a352b8159
snusnu <gamsnjaga@gmail.com>
2010-02-10 04:50:50 +0100

    More strict dependencies in Gemfile

--- rails_3.0.0.beta_gemfile.rb
+++ rails_3.0.0.beta_gemfile.rb
@@ -3,23 +3,25 @@ create_file 'Gemfile' do
 <<-GEMFILE
 source 'http://gemcutter.org'
 
-gem 'rails',        '3.0.0.beta'
+gem 'rails',             '3.0.0.beta'
 
-gem 'data_objects', '0.10.1'
-gem 'do_sqlite3',   '0.10.1'
+gem 'data_objects',      '0.10.1'
+gem 'do_sqlite3',        '0.10.1'
 
 git 'git://github.com/snusnu/dm-core.git', 'branch' => 'active_support'
 git "git://github.com/snusnu/dm-more.git", 'branch' => 'active_support'
+
+gem 'dm-core',           '~> 0.10.2'
+gem 'dm-types',          '~> 0.10.2'
+gem 'dm-validations',    '~> 0.10.2'
+gem 'dm-constraints',    '~> 0.10.2'
+gem 'dm-aggregates',     '~> 0.10.2'
+gem 'dm-timestamps',     '~> 0.10.2'
+gem 'dm-migrations',     '~> 0.10.2'
+gem 'dm-observer',       '~> 0.10.2'
+
 git 'git://github.com/dkubb/rails3_datamapper.git'
 
-gem 'dm-core'
-gem 'dm-types'
-gem 'dm-validations'
-gem 'dm-constraints'
-gem 'dm-aggregates'
-gem 'dm-timestamps'
-gem 'dm-migrations'
-gem 'dm-observer'
-gem 'rails3_datamapper'
+gem 'rails3_datamapper', '~> 0.10.2'
 GEMFILE
 end
--- rails_master_gemfile.rb
+++ rails_master_gemfile.rb
@@ -5,31 +5,34 @@ source 'http://gemcutter.org'
 
 git 'git://github.com/carlhuda/bundler.git'
 
-gem 'bundler'
+gem 'bundler',           '~> 0.9.3'
 
 git 'git://github.com/rails/rails.git'
 
-gem 'activesupport', :require => 'active_support'
-gem 'activemodel',   :require => 'active_model'
-gem 'actionpack',    :require => 'action_pack'
-gem 'actionmailer',  :require => 'action_mailer'
-gem 'railties',      :require => 'rails'
+gem 'activesupport',     '~> 3.0.0.beta1', :require => 'active_support'
+gem 'actionpack',        '~> 3.0.0.beta1', :require => 'action_pack'
+gem 'railties',          '~> 3.0.0.beta1', :require => 'rails'
 
-gem 'data_objects', '0.10.1'
-gem 'do_sqlite3',   '0.10.1'
+# Uncomment this if you need actionmailer
+# gem 'actionmailer',      '~> 3.0.0.beta1', :require => 'action_mailer'
+
+gem 'data_objects',      '~> 0.10.1'
+gem 'do_sqlite3',        '~> 0.10.1'
 
 git 'git://github.com/snusnu/dm-core.git', 'branch' => 'active_support'
 git "git://github.com/snusnu/dm-more.git", 'branch' => 'active_support'
+
+gem 'dm-core',           '~> 0.10.2'
+gem 'dm-types',          '~> 0.10.2'
+gem 'dm-validations',    '~> 0.10.2'
+gem 'dm-constraints',    '~> 0.10.2'
+gem 'dm-aggregates',     '~> 0.10.2'
+gem 'dm-timestamps',     '~> 0.10.2'
+gem 'dm-migrations',     '~> 0.10.2'
+gem 'dm-observer',       '~> 0.10.2'
+
 git 'git://github.com/dkubb/rails3_datamapper.git'
 
-gem 'dm-core'
-gem 'dm-types'
-gem 'dm-validations'
-gem 'dm-constraints'
-gem 'dm-aggregates'
-gem 'dm-timestamps'
-gem 'dm-migrations'
-gem 'dm-observer'
-gem 'rails3_datamapper'
+gem 'rails3_datamapper', '~> 0.10.2'
 GEMFILE
 end
