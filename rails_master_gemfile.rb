d9ffaa8b4c94ff7ee9b0af27f4c153017d130d44
snusnu <gamsnjaga@gmail.com>
2010-02-16 19:31:35 +0100

    Updates rspec git source dependencies to a6 and a7
    This avoids pulling in a5 and a6 from gemcutter
    although git was specified as a source for rspec.

--- rails_master_gemfile.rb
+++ rails_master_gemfile.rb
@@ -55,11 +55,11 @@ git 'git://github.com/rspec/rspec-mocks.git'
 git 'git://github.com/rspec/rspec-rails.git'
 
 group(:test) do
-  gem 'rspec',              '2.0.0.a5'
-  gem 'rspec-core',         '2.0.0.a5', :require => 'rspec/core'
-  gem 'rspec-expectations', '2.0.0.a5', :require => 'rspec/expectations'
-  gem 'rspec-mocks',        '2.0.0.a5', :require => 'rspec/mocks'
-  gem 'rspec-rails',        '2.0.0.a6'
+  gem 'rspec',              '2.0.0.a6'
+  gem 'rspec-core',         '2.0.0.a6', :require => 'rspec/core'
+  gem 'rspec-expectations', '2.0.0.a6', :require => 'rspec/expectations'
+  gem 'rspec-mocks',        '2.0.0.a6', :require => 'rspec/mocks'
+  gem 'rspec-rails',        '2.0.0.a7'
 end
 GEMFILE
 end
