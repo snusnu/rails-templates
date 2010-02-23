ed563379413211ceb6acab3aeb4e19ca40588dd1
snusnu <gamsnjaga@gmail.com>
2010-02-23 16:56:08 +0100

    Require the dm-rails/railtie not rails3_datamapper

--- rails_application_config.rb
+++ rails_application_config.rb
@@ -2,7 +2,7 @@ gsub_file 'config/application.rb', /require 'rails\/all'/ do
 <<-RUBY
 # Pick the frameworks you want:
 require 'action_controller/railtie'
-require 'rails3_datamapper/railtie'
+require 'dm-rails/railtie'
 # require 'action_mailer/railtie'
 # require 'active_resource/railtie'
 # require 'rails/test_unit/railtie'
