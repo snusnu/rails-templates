0391f48b616204d219eb3926bfdbbef5eda2b9dc
snusnu <gamsnjaga@gmail.com>
2010-02-26 14:14:38 +0100

    Bumped rspec dependency to 2.0.0.a9

--- rails_master_gemfile.rb
+++ rails_master_gemfile.rb
@@ -51,10 +51,10 @@ git 'git://github.com/rspec/rspec-mocks.git'
 git 'git://github.com/rspec/rspec-rails.git'
 
 group(:test) do
-  gem 'rspec',              '2.0.0.a8'
-  gem 'rspec-core',         '2.0.0.a8', :require => 'rspec/core'
-  gem 'rspec-expectations', '2.0.0.a8', :require => 'rspec/expectations'
-  gem 'rspec-mocks',        '2.0.0.a8', :require => 'rspec/mocks'
+  gem 'rspec',              '2.0.0.a9'
+  gem 'rspec-core',         '2.0.0.a9', :require => 'rspec/core'
+  gem 'rspec-expectations', '2.0.0.a9', :require => 'rspec/expectations'
+  gem 'rspec-mocks',        '2.0.0.a9', :require => 'rspec/mocks'
   gem 'rspec-rails',        '2.0.0.a9'
 end
 GEMFILE
