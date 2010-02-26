cd07a6ddcff1dce33095dec0f2031147615d8ab6
snusnu <gamsnjaga@gmail.com>
2010-02-26 14:46:17 +0100

    Fixed the dev_server by replacing the correct URL

--- dev_server.rb
+++ dev_server.rb
@@ -5,7 +5,7 @@ get '/:template' do
   buffer = ''
   name = File.expand_path("../#{params[:template]}", __FILE__)
   File.read(name).each do |line|
-    buffer += line.gsub(/http:\/\/github.com\/snusnu\/rails-templates\/raw\/rspec/, 'http://localhost:4567')
+    buffer += line.gsub(/http:\/\/github.com\/snusnu\/rails-templates\/raw\/master/, 'http://localhost:4567')
   end
   buffer
 end
