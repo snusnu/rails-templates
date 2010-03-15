744c624a1544114f851c61b6b748b77ee04f8d6d
snusnu <gamsnjaga@gmail.com>
2010-03-15 13:15:01 +0100

    Bumped rspec dependency to 2.0.0.beta.4

--- rails_master_gemfile.rb
+++ rails_master_gemfile.rb
@@ -51,11 +51,11 @@ git 'git://github.com/rspec/rspec-mocks.git'
 git 'git://github.com/rspec/rspec-rails.git'
 
 group(:test) do
-  gem 'rspec',              '2.0.0.beta.3'
-  gem 'rspec-core',         '2.0.0.beta.3', :require => 'rspec/core'
-  gem 'rspec-expectations', '2.0.0.beta.3', :require => 'rspec/expectations'
-  gem 'rspec-mocks',        '2.0.0.beta.3', :require => 'rspec/mocks'
-  gem 'rspec-rails',        '2.0.0.beta.3'
+  gem 'rspec',              '2.0.0.beta.4'
+  gem 'rspec-core',         '2.0.0.beta.4', :require => 'rspec/core'
+  gem 'rspec-expectations', '2.0.0.beta.4', :require => 'rspec/expectations'
+  gem 'rspec-mocks',        '2.0.0.beta.4', :require => 'rspec/mocks'
+  gem 'rspec-rails',        '2.0.0.beta.4'
 end
 GEMFILE
 end
