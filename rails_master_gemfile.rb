10674fc7afbe4136472b2fe02139caaa825d9d8a
snusnu <gamsnjaga@gmail.com>
2010-04-05 02:59:50 +0200

    New DM repo layout, rails-3.0.0.beta2, rspec-2.0.0.beta.5

--- rails_master_gemfile.rb
+++ rails_master_gemfile.rb
@@ -1,19 +1,17 @@
 remove_file 'Gemfile'
 create_file 'Gemfile' do
 <<-GEMFILE
-source 'http://gemcutter.org'
+source 'http://rubygems.org'
 
 git 'git://github.com/rails/rails.git'
 
-gem 'activesupport',     '~> 3.0.0.beta1', :require => 'active_support'
-gem 'actionpack',        '~> 3.0.0.beta1', :require => 'action_pack'
-gem 'railties',          '~> 3.0.0.beta1', :require => 'rails'
+gem 'activesupport',        '~> 3.0.0.beta2', :require => 'active_support'
+gem 'actionpack',           '~> 3.0.0.beta2', :require => 'action_pack'
+gem 'actionmailer',         '~> 3.0.0.beta2', :require => 'action_mailer'
+gem 'railties',             '~> 3.0.0.beta2', :require => 'rails'
 
-# Uncomment this if you need actionmailer
-# gem 'actionmailer',      '~> 3.0.0.beta1', :require => 'action_mailer'
-
-gem 'data_objects',      '~> 0.10.1'
-gem 'do_sqlite3',        '~> 0.10.1'
+gem 'data_objects',         '~> 0.10.1'
+gem 'do_sqlite3',           '~> 0.10.1'
 
 # You can use any of the other available database adapters.
 # This is only a small excerpt of the list of all available adapters
@@ -24,40 +22,33 @@ gem 'do_sqlite3',        '~> 0.10.1'
 #
 # for a rather complete list of available datamapper adapters and plugins
 
-# gem 'do_mysql',          '~> 0.10.1'
-# gem 'do_postgres',       '~> 0.10.1'
-# gem 'do_oracle',         '~> 0.10.1'
+# gem 'do_mysql',             '~> 0.10.1'
+# gem 'do_postgres',          '~> 0.10.1'
+# gem 'do_oracle',            '~> 0.10.1'
 
-gem 'dm-core',             '~> 0.10.2', :git => 'git://github.com/datamapper/dm-core.git'
+datamapper = 'git://github.com/datamapper'
 
-git "git://github.com/datamapper/dm-more.git" do
-  gem 'dm-types',          '~> 0.10.2'
-  gem 'dm-validations',    '~> 0.10.2'
-  gem 'dm-constraints',    '~> 0.10.2'
-  gem 'dm-aggregates',     '~> 0.10.2'
-  gem 'dm-timestamps',     '~> 0.10.2'
-  gem 'dm-migrations',     '~> 0.10.2'
-  gem 'dm-observer',       '~> 0.10.2'
-end
-
-git 'git://github.com/datamapper/dm-transactions.git'
-gem 'dm-transactions', '~> 0.10.2'
-
-git 'git://github.com/datamapper/dm-rails.git'
-gem 'dm-rails', '~> 0.10.2'
-
-git 'git://github.com/rspec/rspec.git'
-git 'git://github.com/rspec/rspec-core.git'
-git 'git://github.com/rspec/rspec-expectations.git'
-git 'git://github.com/rspec/rspec-mocks.git'
-git 'git://github.com/rspec/rspec-rails.git'
+gem 'dm-core',              '~> 0.10.2', :git => "#{datamapper}/dm-core.git"
+gem 'dm-rails',             '~> 0.10.2', :git => "#{datamapper}/dm-rails.git"
+gem 'dm-types',             '~> 0.10.2', :git => "#{datamapper}/dm-types"
+gem 'dm-validations',       '~> 0.10.2', :git => "#{datamapper}/dm-validations"
+gem 'dm-constraints',       '~> 0.10.2', :git => "#{datamapper}/dm-constraints"
+gem 'dm-transactions',      '~> 0.10.2', :git => "#{datamapper}/dm-transactions.git"
+gem 'dm-aggregates',        '~> 0.10.2', :git => "#{datamapper}/dm-aggregates"
+gem 'dm-timestamps',        '~> 0.10.2', :git => "#{datamapper}/dm-timestamps"
+gem 'dm-migrations',        '~> 0.10.2', :git => "#{datamapper}/dm-migrations"
+gem 'dm-observer',          '~> 0.10.2', :git => "#{datamapper}/dm-observer"
 
 group(:test) do
-  gem 'rspec',              '2.0.0.beta.4'
-  gem 'rspec-core',         '2.0.0.beta.4', :require => 'rspec/core'
-  gem 'rspec-expectations', '2.0.0.beta.4', :require => 'rspec/expectations'
-  gem 'rspec-mocks',        '2.0.0.beta.4', :require => 'rspec/mocks'
-  gem 'rspec-rails',        '2.0.0.beta.4'
+
+  rspec = 'git://github.com/rspec'
+
+  gem 'rspec',              '2.0.0.beta.5', :git => "#{rspec}/rspec.git"
+  gem 'rspec-core',         '2.0.0.beta.5', :git => "#{rspec}/rspec-core.git",         :require => 'rspec/core'
+  gem 'rspec-expectations', '2.0.0.beta.5', :git => "#{rspec}/rspec-expectations.git", :require => 'rspec/expectations'
+  gem 'rspec-mocks',        '2.0.0.beta.5', :git => "#{rspec}/rspec-mocks.git",        :require => 'rspec/mocks'
+  gem 'rspec-rails',        '2.0.0.beta.5', :git => "#{rspec}/rspec-rails.git"
+
 end
 GEMFILE
 end
