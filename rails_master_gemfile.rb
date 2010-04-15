a6c9d38883404873eb66169f7df9c20766e3c494
snusnu <gamsnjaga@gmail.com>
2010-04-15 04:45:03 +0200

    No more problems with rspec-rails master and bundler-0.9.19

--- rails_master_gemfile.rb
+++ rails_master_gemfile.rb
@@ -50,7 +50,7 @@ group(:test) do
   gem 'rspec-core',         '2.0.0.beta.6', :git => "git://github.com/rspec/rspec-core.git",         :require => 'rspec/core'
   gem 'rspec-expectations', '2.0.0.beta.6', :git => "git://github.com/rspec/rspec-expectations.git", :require => 'rspec/expectations'
   gem 'rspec-mocks',        '2.0.0.beta.6', :git => "git://github.com/rspec/rspec-mocks.git",        :require => 'rspec/mocks'
-  gem 'rspec-rails',        '2.0.0.beta.6'  #, :git => "git://github.com/rspec/rspec-rails.git"
+  gem 'rspec-rails',        '2.0.0.beta.6', :git => "git://github.com/rspec/rspec-rails.git"
 
 end
 
